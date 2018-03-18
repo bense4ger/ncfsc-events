26b1f5a6f7da5a67fd4d2af10edc46fada8e327c
Ben Seager <github@benseager.co.uk>
2018-03-18 13:36:04 +0000

    debugging

--- cloudformation.sh
+++ cloudformation.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 aws cloudformation describe-stacks --region $EC2_REGION --stack-name $STACK_NAME | jq '.[0]' | read stacks
-jq '.[0] | .StackId' | read stackId
-
-echo $stackId
+echo $stacks
