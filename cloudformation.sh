5d88e32e0b109c044caf05ba7f25d667b5ff5c2c
Ben Seager <github@benseager.co.uk>
2018-03-18 13:32:37 +0000

    Trying to get stack id

--- cloudformation.sh
+++ cloudformation.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-aws cloudformation describe-stacks --stack-name $STACK_NAME | jq '.[0]' | read stacks
-echo $stacks
+aws cloudformation describe-stacks --region $EC2_REGION --stack-name $STACK_NAME | jq '.[0]' | read stacks
+jq '.[0] | .StackId' | read stackId
+
+echo $stackId
